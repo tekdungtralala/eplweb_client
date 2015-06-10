662f41a6c5e8ffdcaa5c3b5c795a7a26fb952eca
tekdungtralala <tekdung554@gmail.com>
2015-06-10 21:51:49 +0700

    Change google clientId

--- webapp/app/app.constant.js
+++ webapp/app/app.constant.js
@@ -3,7 +3,7 @@
 
 	var youtubeUrl = "http://www.youtube.com/embed/";
 	var googleClientId = 
-		"882702102207-s24ht598ci4dhc7mhafp1f4vu25mhfuh.apps.googleusercontent.com";
+		"443417738207-7mf3g45c6jr8u7md8uiuj93gkgmfnoe4.apps.googleusercontent.com";
 	var facebookAppId = "1615396538705155";
 
 	angular.module("app")
