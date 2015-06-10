c1b232cd89313fef6052c1f7ba48801677e7a306
tekdungtralala <tekdung554@gmail.com>
2015-06-10 22:30:23 +0700

    Change facebookAppId

--- webapp/app/app.constant.js
+++ webapp/app/app.constant.js
@@ -4,7 +4,7 @@
 	var youtubeUrl = "http://www.youtube.com/embed/";
 	var googleClientId = 
 		"443417738207-7mf3g45c6jr8u7md8uiuj93gkgmfnoe4.apps.googleusercontent.com";
-	var facebookAppId = "1615396538705155";
+	var facebookAppId = "894596570608302";
 
 	angular.module("app")
 		.constant("youtubeUrl", youtubeUrl)
